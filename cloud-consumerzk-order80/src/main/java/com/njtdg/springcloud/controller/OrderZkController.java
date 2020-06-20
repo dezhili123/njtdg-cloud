589ee88dee0ba267385e7fe5d19c30fffa2c146d
ldz <879095880@qq.com>
2020-06-20 23:41:24 +0800

    增加zk和consul做为注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/njtdg/springcloud/controller/OrderZkController.java
@@ -0,0 +1,24 @@
+package com.njtdg.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class OrderZkController {
+
+    public static final String INVOKE_URL="http://cloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymetInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        return result;
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/njtdg/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.njtdg.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/consul")
+    private String paymentConsul(){
+        return "springcloud with consul: "+serverPort+"\t   "+ UUID.randomUUID().toString();
+    }
+
+
+}
